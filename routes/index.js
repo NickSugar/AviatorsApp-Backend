8c3999737a214ff3a1af02af5c9b6bb2bcd71632
NickSugar <nicksugar1@outlook.com>
2017-06-07 14:30:22 -0600

    added apidoc

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+ * Basic Example
+ *
+ * This is a basic example for apiDoc.
+ * Documentation blocks without @api (like this block) will be ignored.
+ */
+
+/**
+ * @api {get} /user/:id Get User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The <code>id</code> of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
