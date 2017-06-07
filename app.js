7015105dff188b57d81c8b886f5ecfd5013e8cee
NickSugar <nicksugar1@outlook.com>
2017-06-07 17:36:01 -0600

    started plane routes

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var planes = require('./routes/planes');
 
 var app = express();
 
@@ -18,8 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/apidocs', express.static(path.join(__dirname, 'apidoc')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/planes', planes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- data/queries.js
+++ data/queries.js
@@ -1,54 +1,54 @@
 var database = require('./connection')
 
 module.exports = {
-  getBooks,
-  getBook,
-  editBook,
-  deleteBook,
-  getAuthorsBookID,
-  getAuthors,
-  getAuthor,
-  getBooksAuthorID,
-  deleteAuthor
+  getPlanes
+  // getBook,
+  // editBook,
+  // deleteBook,
+  // getAuthorsBookID,
+  // getAuthors,
+  // getAuthor,
+  // getBooksAuthorID,
+  // deleteAuthor
 }
 
-function getBooks() {
-  return database('book').select('*')
+function getPlanes() {
+  return database('planes').select('*')
 }
 
-function getBook(book_id) {
-  return database('book').select('*').where('id', book_id)
-}
-
-function editBook(book, book_id) {
-  return database('book').where('id', book_id)
-    .update(book)
-}
-
-function deleteBook(book_id) {
-  return database('book').where('id', book_id).del()
-}
-
-function getAuthorsBookID(book_id) {
-  return database('book_author').select('*')
-    .where('book_id', book_id)
-    .join('author', 'book_author.author_id', '=', 'author.id')
-}
-
-function getAuthor(author_id) {
-  return database('author').select('*').where('id', author_id)
-}
-
-function getAuthors() {
-  return database('author').select('*')
-}
-
-function getBooksAuthorID(author_id) {
-  return database('book_author').select('*')
-    .where('author_id', author_id)
-    .join('book', 'book_author.book_id', '=', 'book.id')
-}
-
-function deleteAuthor(author_id) {
-  return database('author').where('id', author_id).del()
-}
+// function getBook(book_id) {
+//   return database('book').select('*').where('id', book_id)
+// }
+//
+// function editBook(book, book_id) {
+//   return database('book').where('id', book_id)
+//     .update(book)
+// }
+//
+// function deleteBook(book_id) {
+//   return database('book').where('id', book_id).del()
+// }
+//
+// function getAuthorsBookID(book_id) {
+//   return database('book_author').select('*')
+//     .where('book_id', book_id)
+//     .join('author', 'book_author.author_id', '=', 'author.id')
+// }
+//
+// function getAuthor(author_id) {
+//   return database('author').select('*').where('id', author_id)
+// }
+//
+// function getAuthors() {
+//   return database('author').select('*')
+// }
+//
+// function getBooksAuthorID(author_id) {
+//   return database('book_author').select('*')
+//     .where('author_id', author_id)
+//     .join('book', 'book_author.book_id', '=', 'book.id')
+// }
+//
+// function deleteAuthor(author_id) {
+//   return database('author').where('id', author_id).del()
+// }
