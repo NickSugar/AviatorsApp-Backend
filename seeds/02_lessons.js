ceb86fa11b6f6797a74230c493a749a62d59b2a2
NickSugar <nicksugar1@outlook.com>
2017-06-08 15:52:06 -0600

    testing seed

--- seeds/02_lessons.js
+++ seeds/02_lessons.js
@@ -7,9 +7,7 @@ exports.seed = function(knex, Promise) {
           number: 1,
           name: 'General aircraft info',
           description: 'This Airplane Flight Manual is intended to provide pilots and instructors with
-
               information for safe and effective operation of this airplane which belongs in the
-
               Light Sport Airplane category. Some supplementary information is also
 
               introduced into the content by the airplane manufacturer. It is the pilot’s
