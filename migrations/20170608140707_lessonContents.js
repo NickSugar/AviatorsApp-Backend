35d03916192039d70dd93d081de98998afc4dd9a
NickSugar <nicksugar1@outlook.com>
2017-06-08 18:50:59 -0600

    fixed seeds... hopfully....again...again

--- migrations/20170608140707_lessonContents.js
+++ migrations/20170608140707_lessonContents.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
     table.integer('lesson_id')
       .references('lessons.id')
       .onDelete('CASCADE');
-    table.decimal('order', 2, 2)
+    table.decimal('order')
     table.string('type')
     table.text('content')
   })
